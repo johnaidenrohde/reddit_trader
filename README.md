51103226960767ca7d7ea2c678730799b1ce7276
aiden <aiden@safe7y.com>
2021-01-20 16:25:51 -0700

    Update

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ We've decided to take investment advice from the good people of reddit. Specific
 
 This app scrapes reddit and makes buy and sell decisions based on the numbers of mentions of a particular ticker. 
 
+Inspired by:
+- https://github.com/RyanElliott10/wsbtickerbot
+- https://www.swaggystocks.com/dashboard/wallstreetbets/ticker-sentiment
+- https://www.reddit.com/r/wallstreetbets/comments/l1839h/gme_announcement/?utm_source=share&utm_medium=mweb
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import praw
+import config
 
+def main():
+    # Setup the reddit scraper
+    reddit = praw.Reddit(client_id=config.reddit_client_id, client_secret=config.reddit_client_secret, user_agent=config.reddit_user_agent)
 
-def main(name):
-
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    # get posts from subreddits
+    hot_posts = reddit.subreddit('wallstreetbets').hot(limit=100)
+    for post in hot_posts:
+        print(post.title)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
